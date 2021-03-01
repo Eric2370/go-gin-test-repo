dd0b258e2ec8e895f97c53c303e45b3b66061588
Qiren Feng <qirenfeng1@gamil.com>
2021-03-01 12:05:23 +0800

    go gin test First commit

---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	/*
+		fmt.Println("Hello, World!")
+	 */
+	fmt.Println("Hello, World!")
+	var x float64 = 20.2
+	y := 42
+	y = 222
+	z := 111
+	fmt.Println(float64(x))
+	fmt.Println(y)
+	fmt.Printf("x is of type %T\n", x)
+	fmt.Printf("y is of type %T\n", y)
+	//for y > 100{
+	//	if y % 2 == 0 {
+	//		fmt.Println(y)
+	//	}
+	//	y--
+	//}
+	fmt.Println(max(z, y, x))
+
+	country := [3]string{"nigeria", "egypt", "sweden"}
+	fmt.Println("Before function call: ", country[0])
+	// [nigeria egypt germany]
+	fmt.Println("After function call: ", country[1])
+	// [nigeria egypt sweden]
+
+	//myProfile := profile{
+	//	Age:  15,
+	//	Name: "Adeshina",
+	//	Salary: 300,
+	//	TechInterest: false,
+	//}
+	myProfile := profile{
+		name: "hi",
+		age : 12,
+	}
+	fmt.Println(getValue(&myProfile))
+
+	numbers := []int {0, 0, 0, 0, 0}
+	numbers = make([]int, 5, 63)
+	printSlice(numbers)
+}
+
+type profile struct {
+	name string
+	age  int
+}
+//func modifyStruct(p profile) profile {
+//	p.Age = 85
+//	p.Name = "Balqees"
+//	p.Salary = 500.45
+//	p.TechInterest = true
+//
+//	return p
+//}
+
+func getValue(profile *profile) int {
+	fmt.Println(*profile)
+	return 1
+}
+
+/* function returning the max between two numbers */
+func max(num1, num2 int, num3 float64) int {
+	/* local variable declaration */
+	var result int
+
+	if (num1 > num2) {
+		result = num1
+	} else {
+		result = num2
+	}
+	return result
+}
+
+func printSlice(x []int) {
+	fmt.Println("len = %d cap = %d slice = %v\n", len(x), cap(x), x)
+	fmt.Println("", len(x), cap(x), x)
+}
